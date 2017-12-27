60e2693b4226d0b6c9822752df68ce681a5262f6
Szymon Piechaczek <piechaczek.szymon@gmail.com>
2017-12-27 01:22:54 +0100

    IX

--- src/pl/agh/edu/hibernate/Main.java
+++ src/pl/agh/edu/hibernate/Main.java
@@ -16,45 +16,23 @@ public class Main {
 
         Transaction tx = session.beginTransaction();
         Supplier supplier = new Supplier("test company 1", "test street", "Krakow");
-        Supplier supplier2 = new Supplier("test company 2", "test street", "Krakow");
 
         Category cat = new Category("test category");
-
-        supplier.addProduct(new Product("test prod 1", 1, cat));
-        supplier.addProduct(new Product("test prod 2", 2, cat));
-        supplier.addProduct(new Product("test prod 3", 3, cat));
-        supplier.addProduct(new Product("test prod 4", 4, cat));
-        Product product = new Product("test prod 5", 5, cat);
-        Product product2 = new Product("test prod 6", 5, cat);
-        supplier2.addProduct(product);
-        supplier2.addProduct(product2);
-
         session.save(supplier);
-        session.save(supplier2);
-
-        SalesTransaction salesTransaction = new SalesTransaction(2313, 2);
-        salesTransaction.addProduct(product2);
-        SalesTransaction salesTransaction2 = new SalesTransaction(2314, 2);
-        salesTransaction2.addProduct(product2);
-        salesTransaction2.addProduct(product);
 
 
-        session.save(salesTransaction);
-        session.save(salesTransaction2);
+        Product testProduct = new Product("test prod cascade source", 5, cat);
+        testProduct.setSupplier(supplier);
 
-        SalesTransaction testTransaction = session.get(SalesTransaction.class, salesTransaction2.getTransactionId());
-        Product testProduct = session.get(Product.class, product2.getDbId());
+        testProduct.addTransaction(new SalesTransaction(1234, 1));
+        testProduct.addTransaction(new SalesTransaction(1235, 2));
+        session.save(testProduct); // saves product and transactions
 
-        System.out.println("Produkty sprzedane w ramach przykładowej transakcji #"
-                + testTransaction.getTransactionNumber() + ":");
-        for (Product p : testTransaction.getProducts()) {
-            System.out.println(p.getProductName());
-        }
 
-        System.out.println("Transakcje, w ramach których sprzedany był produkt " + testProduct.getProductName());
-        for (SalesTransaction t : testProduct.getSalesTransactions()) {
-            System.out.println(String.valueOf(t.getTransactionNumber()));
-        }
+        SalesTransaction testTransaction = new SalesTransaction(2345, 5);
+        testTransaction.addProduct(new Product("test prod cascade 1", 3, supplier, cat));
+        testTransaction.addProduct(new Product("test prod cascade 2", 2, supplier, cat));
+        session.save(testTransaction); // saves transaction and products
 
         tx.commit();
         session.close();
